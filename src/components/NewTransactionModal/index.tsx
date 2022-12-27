dbae26fa3c156cc3e9cd8cb0930a5ff873192492
MarianoArthur <marianocapiliku@gmail.com>
2022-12-27 02:04:20 +0100

    ModalComponentStyling

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Overlay, Content } from "./styles";
+import { X } from "phosphor-react";
+import { Overlay, Content, CloseButton } from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -9,6 +10,10 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova transacao</Dialog.Title>
 
+        <CloseButton>
+          <X size={22} />
+        </CloseButton>
+
         <form action="">
           <input type="text" placeholder="Descricao" required />
           <input type="number" placeholder="Preco" required />
@@ -16,7 +21,6 @@ export function NewTransactionModal() {
 
           <button type="submit"> Cadastrar</button>
         </form>
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   );
